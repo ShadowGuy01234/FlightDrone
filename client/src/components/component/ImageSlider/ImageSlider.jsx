bc18c81c3c4979b1bcb78c50a6e54a475a29a6da
SinghUjjwal12 <135581140+SinghUjjwal12@users.noreply.github.com>
2025-01-03 23:31:42 +0530

    Update8.0
    Home page UI updated and Dashboard is in progress.. (routed on - /dashboard/admin)

--- client/src/components/component/ImageSlider/ImageSlider.jsx
+++ client/src/components/component/ImageSlider/ImageSlider.jsx
@@ -1,46 +1,82 @@
 import React, { useState } from 'react';
-import './ImageSlider.css'; 
+import './ImageSlider.css';
 
 const ImageSlider = () => {
-  
-  const images = [
+
+  const data = [
     { src: '/003.jpg', name: 'Mike', title: 'web3 Developer' },
-    { src: '/003.jpg', name: 'Samite', title: 'wordpress Developer' },
-    { src: '/003.jpg', name: 'Hashi', title: 'java Developer' },
-    { src: '/003.jpg', name: 'Kaity', title: 'web Developer' },
-    { src: '/003.jpg', name: 'Lauren', title: 'php Developer' },
-    { src: '/003.jpg', name: 'Ryan', title: 'seo Developer' },
-    { src: '/003.jpg', name: 'Dakes', title: 'sql Developer' },
+    { src: '/003.jpg', name: 'Samite', title: 'WordPress Developer' },
+    { src: '/003.jpg', name: 'Hashi', title: 'Java Developer' },
+    { src: '/003.jpg', name: 'Kaity', title: 'Web Developer' },
   ];
 
- 
-  const [activeIndex, setActiveIndex] = useState(3); 
-
- 
-  const handleImageClick = (index) => {
-    setActiveIndex(index);
-  };
-
   return (
-    <section className="slider-container">
-      <div className="slider-images">
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className={`slider-img ${activeIndex === index ? 'active' : ''}`}
-            onClick={() => handleImageClick(index)}
-          >
-            <img src={image.src} alt={image.name} />
-            <h1 className='hh'>{image.name}</h1>
-            <div className="details">
-              <h2>{image.name}</h2>
-              <p>{image.title}</p>
+    <div className="wrapper1">
+      <div className="container1">
+        <input type="radio" name="slide" id="c1" defaultChecked />
+        <label htmlFor="c1" className="card1">
+          <div className="row1">
+            <div className="icon1">1</div>
+            <div className="description1">
+              <h4>Winter</h4>
+              <p>Winter has so much to offer - creative activities</p>
             </div>
           </div>
-        ))}
+        </label>
+        <input type="radio" name="slide" id="c2" />
+        <label htmlFor="c2" className="card1">
+          <div className="row1">
+            <div className="icon1">2</div>
+            <div className="description1">
+              <h4>Digital Technology</h4>
+              <p>Gets better every day - stay tuned</p>
+            </div>
+          </div>
+        </label>
+        <input type="radio" name="slide" id="c3" />
+        <label htmlFor="c3" className="card1">
+          <div className="row1">
+            <div className="icon1">3</div>
+            <div className="description1">
+              <h4>Globalization</h4>
+              <p>Help people all over the world</p>
+            </div>
+          </div>
+        </label>
+        <input type="radio" name="slide" id="c4" />
+        <label htmlFor="c4" className="card1">
+          <div className="row1">
+            <div className="icon1">4</div>
+            <div className="description1">
+              <h4>New technologies</h4>
+              <p>Space engineering becomes more and more advanced</p>
+            </div>
+          </div>
+        </label>
+        {/* <input type="radio" name="slide" id="c5" />
+        <label htmlFor="c5" className="card1">
+          <div className="row1">
+            <div className="icon1">5</div>
+            <div className="description1">
+              <h4>Digital Technology</h4>
+              <p>Gets better every day - stay tuned</p>
+            </div>
+          </div>
+        </label>
+        <input type="radio" name="slide" id="c6" />
+        <label htmlFor="c6" className="card1">
+          <div className="row1">
+            <div className="icon1">6</div>
+            <div className="description1">
+              <h4>Digital Technology</h4>
+              <p>Gets better every day - stay tuned</p>
+            </div>
+          </div>
+        </label> */}
       </div>
-    </section>
+    </div>
   );
 };
+
 
 export default ImageSlider;
